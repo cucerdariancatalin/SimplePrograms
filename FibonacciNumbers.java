4bb6e8ee1303bac19390d2bc65afde6e62129620
Vitas <salvantesvitas@gmail.com>
2021-04-09 20:26:53 +0200

    Simplified implementation

--- FibonacciNumbers.java
+++ FibonacciNumbers.java
@@ -6,55 +6,30 @@ import java.util.Scanner;
  * @author VitasSalvantes
  * @version 1.0
  */
-
 public class FibonacciNumbers {
-
-    /** Previous calculated number */
-    private int past = 0;
-
-    /** Number calculated in this iteration */
-    private int present = 0;
-
-    /** The final result of finding the Fibonacci numbers */
-    private int result;
-
-    /** The number of Fibonacci numbers displayed */
-    private int lgth;
-
-    /** A Scanner class object that accepts user input */
-    private Scanner sc;
 
     /** Method that launches the program */
     public static void main(String[] args) {
-        FibonacciNumbers fn = new FibonacciNumbers();
-        fn.run();
-    }
-
-    /**
-     * A method that calls a dialog with the user and accepts the number of
-     * Fibonacci numbers from the user
-     */
-    private void run() {
         System.out.println("- How many numbers?");
+        int numberOfNumbers = new Scanner(System.in).nextInt();
 
-        sc = new Scanner(System.in);
-        lgth = sc.nextInt();
-        sc.close();
-
-        makeNumbers();
+        System.out.println(new FibonacciNumbers().makeFibonacciNumbers(numberOfNumbers));
     }
 
-    /** Method generating Fibonacci numbers */
-    private void makeNumbers() {
-        System.out.print(present + " ");
-        present = 1;
+    /** Method that generates Fibonacci numbers */
+    private String makeFibonacciNumbers(int numberOfNumbers) {
+        String fibonacciNumbers = "Your numbers: 1 ";
+        int nextNumber = 1, previousNumber = 0, currentNumber, counter = numberOfNumbers;
 
-        while (lgth > 0) {
-            result = present;
-            present += past;
-            past = result;
-            System.out.print(result + " ");
-            lgth--;
+        while (counter > 1) {
+            currentNumber = previousNumber + nextNumber;
+            previousNumber = nextNumber;
+            nextNumber = currentNumber;
+            fibonacciNumbers += currentNumber + " ";
+            
+            counter--;
         }
+
+        return fibonacciNumbers;
     }
 }
