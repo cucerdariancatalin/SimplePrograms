aba141fa480a0c55104eebfd7ba3b407c6a4530a
Vitas <salvantesvitas@gmail.com>
2021-04-28 19:59:35 +0200

    Simplified implementation

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -5,61 +5,26 @@
  * three and five at the same time, it prints "FizzBuzz".
  * 
  * @author VitasSalvantes
- * @version 1.0
+ * @version 2.0
  */
 
 public class FizzBuzz {
 
-    /** Text for multiples of 5 */
-    private String buzz = "Buzz ";
-
-    /** Text for multiples of 3 */
-    private String fizz = "Fizz ";
-
-    /** Text for multiples of 15 (== 3 * 5) */
-    private String fizzBuzz = "FizzBuzz ";
-
-    /** Array with numbers from 0 to 100 */
-    private int[] numbers;
-
-    /** Method that launches the program */
+/** Method that launches the program */
     public static void main(String[] args) {
-        FizzBuzz fb = new FizzBuzz();
-        fb.printer(fb.fizzBuzzer());
-    }
-
-    /** Method for printing the sequence to the console */
-    private void printer(int[] numbers) {
-        for (int i : numbers) {
+        for (int i = 1; i < 101; i++) {
 
             if (i % 15 == 0) {
-                System.out.print(fizzBuzz);
+                System.out.print("FizzBuzz ");
                 continue;
             }
 
             if (i % 5 == 0) {
-                System.out.print(buzz);
+                System.out.print("Buzz ");
                 continue;
             }
 
-            if (i % 3 == 0) {
-                System.out.print(fizz);
-                continue;
-            }
-
-            System.out.print(i + " ");
+            if (i % 3 == 0) System.out.print("Fizz "); else System.out.print(i + " ");
         }
-    }
-
-    /**
-     * Method for filling an array {@link FizzBuzz#numbers} with numbers from 0 to
-     * 100
-     */
-    private int[] fizzBuzzer() {
-        numbers = new int[100];
-        for (int i = 0; i < 100; i++) {
-            numbers[i] = i + 1;
-        }
-        return numbers;
     }
 }
