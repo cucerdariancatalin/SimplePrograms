d52e5b4656db4882d5c343951aa34c1fd1f465db
VitasSalvantes <salvantesvitas@gmail.com>
2021-07-26 14:14:39 +0200

    Work has begun on creating a program "HelloWorld"

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Traditionally the first program for all programmers
+ *
+ * @author VitasSalvantes
+ * @version 1.0
+ */
+public class HelloWorld {
+
+    /**
+     * Method that launches the program
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
