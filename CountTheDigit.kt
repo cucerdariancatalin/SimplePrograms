c290438a99ec7273ac3afcdd32fd25df476facc7
VitasSalvantes <salvantesvitas@gmail.com>
2021-09-05 17:52:00 +0200

    Added documentation

--- CountTheDigit.kt
+++ CountTheDigit.kt
@@ -1,14 +1,26 @@
+/**
+ * Run a program that counts the number of repetitions of a digit in range of numbers.
+ *
+ * @author VitasSalvantes
+ */
 fun main() {
-    val maxNumber = 140_132_412
-    val soughtDigit = 1
+    // Largest number in range
+    val largestNumber = 140_132_412
 
+    // Desired digit
+    val desiredDigit = 1
+
+    // Number of repetitions of a digit
     var counter = 0
 
-    for (number in 0..maxNumber) {
+    /* Each number in the range is represented as a string, and the desired digit is represented as a character,
+    after which is counted the number of repetitions of a character in a string */
+    for (number in 0..largestNumber) {
         for (char in "$number") {
-            if (char == soughtDigit.digitToChar()) counter++
+            if (char == desiredDigit.digitToChar()) counter++
         }
 
+        // If the largest number is huge, then the program can take quite a long time
         when (number) {
             1_000_000 -> println("Please wait...")
             100_000_000 -> println("Please wait longer...")
@@ -19,5 +31,5 @@ fun main() {
         }
     }
 
-    println("The digit $soughtDigit occurs in sequence from 0 to $maxNumber $counter times")
+    println("The digit $desiredDigit occurs in sequence from 0 to $largestNumber $counter times")
 }
