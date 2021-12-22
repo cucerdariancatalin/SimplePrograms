c23f0e81d8d049a254a3650e137944c1fc1de514
VitasSalvantes <salvantesvitas@gmail.com>
2021-12-22 14:19:52 +0100

    Improved code

--- Polynomial.kt
+++ Polynomial.kt
@@ -1,7 +1,5 @@
-import kotlin.test.todo
-
 /**
- * Run the program that finds the integer roots of a polynomial
+ * Run the program that finds the integer roots of a polynomial using Horner's method.
  *
  * @author VitasSalvantes
  */
@@ -9,23 +7,21 @@ import kotlin.test.todo
 // A, B, C, D, E - coefficients
 // x - variable
 // Ax^4 != 0 => 4 roots
-// TODO: 16.09.2021 Create class polynomial
 fun main() {
     // 0x^4 + 0x^3 + x^2 - x - 2 = 0, roots: -1, 2
     // Coefficients
-    // TODO: 16.09.2021 Add user's input
-    val A = 0
-    val B = 0
-    val C = 1
-    val D = -1
-    val E = -2
-    val coefficients = listOf(A, B, C, D, E)
+    val coefficientA = 0
+    val coefficientB = 0
+    val coefficientC = 1
+    val coefficientD = -1
+    val coefficientE = -2
+    val coefficients = listOf(coefficientA, coefficientB, coefficientC, coefficientD, coefficientE)
 
     // Minimum value of the checkable root
-    var minEquationRoot = -2
+    val minEquationRoot = -2
 
     // Maximum value of the checkable root
-    var maxEquationRoot = 3
+    val maxEquationRoot = 3
 
     // "Correct" roots
     val roots = mutableSetOf<Int>()
@@ -48,7 +44,6 @@ fun main() {
     if (roots.isNotEmpty()) {
         println(roots.sorted())
     } else {
-        TODO("Add setting a new range")
         println(
             "There are no integer roots in $minEquationRoot..$maxEquationRoot range. " +
                     "Check the correctness of the input and try to increase the range."
@@ -57,13 +52,13 @@ fun main() {
 }
 
 /**
- * Root check using Horner's scheme
+ * Root check using Horner's method.
  */
 fun checkRoot(coefficients: List<Int>, verifiableRoot: Int): Boolean {
     // The sum of the product of the root by the current value (according to the scheme) and the next coefficient
     var result = 0
 
-    // See Horner's scheme
+    // See Horner's method
     for (coefficient in coefficients) {
         result = verifiableRoot * result + coefficient
     }
