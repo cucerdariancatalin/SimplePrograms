eda1a102be27de72c4739347e0d4337b8998f021
VitasSalvantes <salvantesvitas@gmail.com>
2022-05-15 13:30:47 +0200

    Added the "factorial" bash script

---
+++ factorial.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# The script calculates the factorial of the argument
+echo "File: $0"
+echo "Number of arguments: $#"
+echo
+
+if [ $# -ne 1 ]; then
+  echo "The number of the arguments must be 1!"
+  exit 1
+fi
+
+if [ "$1" -lt 0 ]; then
+  echo "The number must not be negative!"
+  exit 1
+fi
+
+number=$1
+factorial=1
+
+for ((i = number; i > 1; i--)); do
+  ((factorial *= i))
+done
+
+echo "$number! = $factorial"
+
+exit 0
