06cb26f778e920f70c195c25d709e57807428243
VitasSalvantes <salvantesvitas@gmail.com>
2022-06-05 22:01:47 +0200

    Added box validation and updated version

--- src/main/java/com/simpleprograms/BoxComparator.java
+++ src/main/java/com/simpleprograms/BoxComparator.java
@@ -1,10 +1,14 @@
 package com.simpleprograms;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
 /**
  * The program to compare two boxes.
  *
  * @author VitasSalvantes
- * @version 2.0
+ * @version 2.5
  */
 public class BoxComparator {
 
@@ -13,12 +17,16 @@ public class BoxComparator {
      *
      * @param box1 the box to be compared with box2. It must have 3 elements: width, length and height.
      * @param box2 the box to be compared with box1. It must have 3 elements: width, length and height.
-     * @return the "Box 1 > Box 2" string if the second box can be nested within the box1;
-     * the "Box 1 < Box 2" string if the box1 can be nested within the second box;
+     * @return the "Box 1 > Box 2" string if the second box can be nested within the box1;<br>
+     * the "Box 1 < Box 2" string if the box1 can be nested within the second box;<br>
      * the "Incompatible" string if the boxes can not be nested within each other.
      */
-    public String checkNestable(int[] box1,
-                                int[] box2) {
+    public static @NotNull String checkNestable(final int @NotNull [] box1, final int @NotNull [] box2) {
+        validateBox(box1);
+        validateBox(box2);
+
+        Arrays.sort(box1);
+        Arrays.sort(box2);
 
         int counterBox2Bigger = 0;
         int counterEqualBoxes = 0;
@@ -43,16 +51,32 @@ public class BoxComparator {
     }
 
     /**
-     * The method launches the program.
+     * Validate a box.
+     *
+     * @param box the box to be validated.
+     * @throws IllegalArgumentException if the box is null or unsuitable.
+     */
+    private static void validateBox(final int[] box) {
+        if (box == null) {
+            throw new IllegalArgumentException("The box must not be null.");
+        }
+
+        if (box.length != 3) {
+            throw new IllegalArgumentException("The box must have 3 elements: width, length and height");
+        }
+
+        for (int dimension : box) {
+            if (dimension < 1) {
+                throw new IllegalArgumentException("Each dimension of the box must be greater than 0.");
+            }
+        }
+    }
+
+
+    /**
+     * The example of using the program.
      */
     public static void main(String[] args) {
-        final var comparator = new BoxComparator();
-
-        System.out.println(comparator.checkNestable(new int[]{1, 2, 3}, new int[]{3, 2, 1}));
-        System.out.println(comparator.checkNestable(new int[]{1, 2, 3}, new int[]{4, 6, 5}));
-        System.out.println(comparator.checkNestable(new int[]{4, 6, 5}, new int[]{1, 2, 3}));
-        System.out.println(comparator.checkNestable(new int[]{3, 2, 1}, new int[]{4, 2, 8}));
-        System.out.println(comparator.checkNestable(new int[]{8, 2, 4}, new int[]{2, 9, 1}));
-
+        System.out.println(BoxComparator.checkNestable(new int[]{1, 2, 3}, new int[]{3, 2, 1}));
     }
 }
