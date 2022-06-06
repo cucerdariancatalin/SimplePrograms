b22845c217644f0b9c0bc2ed1c6b9a1c145ecb54
VitasSalvantes <salvantesvitas@gmail.com>
2022-06-06 18:05:53 +0200

    Added the MIT license

--- src/test/java/com/simpleprograms/BoxComparatorTest.java
+++ src/test/java/com/simpleprograms/BoxComparatorTest.java
@@ -1,3 +1,27 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2021 VitasSalvantes
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.simpleprograms;
 
 import org.junit.jupiter.api.AfterEach;
@@ -21,8 +45,8 @@ class BoxComparatorTest {
 
     @BeforeEach
     void setUp() {
-        testBox1 = new int[] {1, 2, 3};
-        testBox2 = new int[] {4, 5, 6};
+        testBox1 = new int[]{1, 2, 3};
+        testBox2 = new int[]{4, 5, 6};
     }
 
     @AfterEach
@@ -44,8 +68,8 @@ class BoxComparatorTest {
         testBox2 = testBox1;
         assertEquals(incompatible, BoxComparator.checkNestable(testBox1, testBox2));
 
-        testBox1 = new int[] {3, 2, 1};
-        testBox2 = new int[] {5, 6, 4};
+        testBox1 = new int[]{3, 2, 1};
+        testBox2 = new int[]{5, 6, 4};
         assertEquals(box2IsGreater, BoxComparator.checkNestable(testBox1, testBox2));
 
         testBox1 = new int[]{4, 2, 8};
@@ -62,7 +86,7 @@ class BoxComparatorTest {
         testBox1 = null;
         assertThrows(IllegalArgumentException.class, () -> BoxComparator.checkNestable(testBox1, testBox2));
 
-        testBox1 = new int[] {1, 2, 3};
+        testBox1 = new int[]{1, 2, 3};
         testBox2 = null;
         assertThrows(IllegalArgumentException.class, () -> BoxComparator.checkNestable(testBox1, testBox2));
 
@@ -74,15 +98,15 @@ class BoxComparatorTest {
         testBox1 = new int[0];
         assertThrows(IllegalArgumentException.class, () -> BoxComparator.checkNestable(testBox1, testBox2));
 
-        testBox1 = new int[] {1, 2, 3};
+        testBox1 = new int[]{1, 2, 3};
         testBox2 = new int[2];
         assertThrows(IllegalArgumentException.class, () -> BoxComparator.checkNestable(testBox1, testBox2));
 
-        testBox2 = new int[] {1, -2, 3};
+        testBox2 = new int[]{1, -2, 3};
         assertThrows(IllegalArgumentException.class, () -> BoxComparator.checkNestable(testBox1, testBox2));
 
-        testBox1 = new int[] {1, 2, 0};
-        testBox2 = new int[] {1, 2, 3};
+        testBox1 = new int[]{1, 2, 0};
+        testBox2 = new int[]{1, 2, 3};
         assertThrows(IllegalArgumentException.class, () -> BoxComparator.checkNestable(testBox1, testBox2));
     }
 }
