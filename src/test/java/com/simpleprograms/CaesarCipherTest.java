c690c3bb8e6904916dc509d529cabbc9bbf266a3
VitasSalvantes <salvantesvitas@gmail.com>
2022-07-01 13:53:20 +0200

    Removed unused import

--- src/test/java/com/simpleprograms/CaesarCipherTest.java
+++ src/test/java/com/simpleprograms/CaesarCipherTest.java
@@ -24,7 +24,6 @@
 
 package com.simpleprograms;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
