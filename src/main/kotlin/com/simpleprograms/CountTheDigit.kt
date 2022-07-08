08b76f14841e5a57a24805619506b43c60b77793
VitasSalvantes <salvantesvitas@gmail.com>
2022-07-08 10:03:20 +0200

    Added license and removed useless comments

--- src/main/kotlin/com/simpleprograms/CountTheDigit.kt
+++ src/main/kotlin/com/simpleprograms/CountTheDigit.kt
@@ -1,22 +1,42 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 VitasSalvantes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.simpleprograms
 
 /**
- * Run a program that counts the number of repetitions of a digit in range of numbers.
+ * The program to count the number of repetitions of a digit in range of numbers.
  *
  * @author VitasSalvantes
+ * @version 1.0.1
  */
 fun main() {
-    // Largest number in range
     val largestNumber = 140_132_412
 
-    // Desired digit
     val desiredDigit = 1
 
-    // Number of repetitions of a digit
     var counter = 0
 
-    /* Each number in the range is represented as a string, and the desired digit is represented as a character,
-    after which is counted the number of repetitions of a character in a string */
     for (number in 0..largestNumber) {
         for (char in "$number") {
             if (char == desiredDigit.digitToChar()) counter++
