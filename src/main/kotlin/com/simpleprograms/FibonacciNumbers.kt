30e0befd2c79cb3effe769dfe1ae95abf582dba8
VitasSalvantes <salvantesvitas@gmail.com>
2022-07-09 11:15:18 +0200

    Added copyright and improved code logic

--- src/main/kotlin/com/simpleprograms/FibonacciNumbers.kt
+++ src/main/kotlin/com/simpleprograms/FibonacciNumbers.kt
@@ -1,10 +1,34 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 VitasSalvantes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.simpleprograms
 
 /**
- * Example of Fibonacci numbers implementation
- * 
+ * Simple implementation of the Fibonacci numbers.
+ *
  * @author VitasSalvantes
- * @version 1.0
+ * @version 2.0.0
  */
 fun main() {
     println("- How many numbers?")
@@ -12,22 +36,18 @@ fun main() {
     print(makeFibonacciNumbers(numberOfNumbers))
 }
 
-/** Method that generates Fibonacci numbers */
-fun makeFibonacciNumbers(numberOfNumbers: Int): String {
-    var fibonacciNumbers: String = "Your numbers: 1 "
-    var nextNumber = 1
-    var previousNumber = 0
-    var currentNumber: Int
-    var counter = numberOfNumbers
+/**
+ * Computes Fibonacci numbers.
+ */
+fun makeFibonacciNumbers(maxIndex: Int) {
+    var nextNumber = 0
+    var sum = 1
 
-    while (counter > 1) {
-        currentNumber = previousNumber + nextNumber
-        previousNumber = nextNumber
-        nextNumber = currentNumber
-        fibonacciNumbers += "$currentNumber "
+    for (index in 1..maxIndex) {
+        print("$nextNumber" + if (index == maxIndex) "\n" else ", ")
 
-        counter--
+        val currentNumber = nextNumber
+        nextNumber = sum
+        sum += currentNumber
     }
-
-    return fibonacciNumbers
 }
