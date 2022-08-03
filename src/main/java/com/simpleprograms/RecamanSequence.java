2fb9ab0301381d25e63001f650393f912046ad26
Ivan Bobrov <salvantesvitas@gmail.com>
2022-08-03 18:02:05 +0200

    Improved code logic

--- src/main/java/com/simpleprograms/RecamanSequence.java
+++ src/main/java/com/simpleprograms/RecamanSequence.java
@@ -1,63 +1,78 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Ivan Bobrov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package com.simpleprograms;
 
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
- * Class with methods that implement the Recaman sequence
- * 
- * @author VitasSalvantes
- * @version 1.5
+ * Simple implementation of the Recaman sequence.
+ *
+ * @author Ivan Bobrov
+ * @version 1.6.0
  */
 
 public class RecamanSequence {
 
-    /** The current member of the sequence */
-    private int number = 0;
+    /**
+     * Prints the Recaman's sequence.
+     *
+     * @param quantity the number of elements to be printed.
+     */
+    public static void printRecamanSequence(final int quantity) {
+        validateQuantity(quantity);
 
-    /** Numbers included in the sequence */
-    final private LinkedList<Integer> sequence = new LinkedList<Integer>();
+        final var recamanSequence = new ArrayList<Integer>(quantity);
+        recamanSequence.add(0);
 
-    /** Sequence length */
-    private int length;
+        for (int i = 1; i < quantity; i++) {
+            final int newElementCandidate = recamanSequence.get(i - 1) - i;
+            recamanSequence.add(newElementCandidate > 0 && !recamanSequence.contains(newElementCandidate)
+                                ? newElementCandidate
+                                : recamanSequence.get(i - 1) + i);
 
-    /** A Scanner class object that accepts user input */
-    final private Scanner sc = new Scanner(System.in);
+            System.out.printf("%d%s", recamanSequence.get(i), i == quantity - 1 ? "\n" : ", ");
+        }
+    }
 
-    /** Method that launches the program */
+    /**
+     * Validates a quantity.
+     *
+     * @param quantity the quantity to be validated.
+     */
+    private static void validateQuantity(final int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("The quantity must be greater than 0");
+        }
+    }
+
+
+
+    /**
+     * The example of using the program.
+     */
     public static void main(String[] args) {
-        RecamanSequence rs = new RecamanSequence();
-
-        rs.setLength();
-        rs.createSequence();
+        RecamanSequence.printRecamanSequence(20);
     }
 
-    /** Setter for {@link RecamanSequence#length} */
-    public void setLength() {
-        System.out.println("- How many numbers?");
-        length = sc.nextInt();
-        sc.close();
-    }
-
-    /** Method that generates the next {@link RecamanSequence#number} */
-    private void addNumber() {
-        if (sequence.getLast() - number <= 0 || sequence.contains(sequence.getLast() - number)) {
-            sequence.addLast(sequence.getLast() + number);
-        } else {
-            sequence.addLast(sequence.getLast() - number);
-        }
-    }
-
-    /** Method that creates a sequence and displays it */
-    private void createSequence() {
-        sequence.addLast(0);
-        number++;
-
-        while (number != length) {
-            addNumber();
-            number++;
-        }
-
-        System.out.println(sequence);
-    }
 }
